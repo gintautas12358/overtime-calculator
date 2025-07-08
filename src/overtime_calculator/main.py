d75514a73e08b0a76b2a343ba27e8aff40f82a29
gpalinauskas <gpa@robominds.de>
2025-07-08 14:30:16 +0200

    add break substraction to the calculations

--- src/overtime_calculator/main.py
+++ src/overtime_calculator/main.py
@@ -3,37 +3,62 @@ from datetime import timedelta
 from pathlib import Path
 from argparse import ArgumentParser
 
+DAY_TO_HOURS = 24
+MIN_TO_SECONDS = 60
+HOURS_TO_MINUTES = 60
+HOURS_TO_SECONDS = HOURS_TO_MINUTES * MIN_TO_SECONDS
+WORKING_DAY_DURATION_H = 8
+WORKING_DAY_DURATION_S = WORKING_DAY_DURATION_H * HOURS_TO_SECONDS
+BREAK_TIME_MIN = 30
+BREAK_TIME_SECONDS = BREAK_TIME_MIN * MIN_TO_SECONDS
 
-def hours_minutes(td: timedelta) -> tuple[int, int]:
-    return td.days * 24 + td.seconds // 3600, (td.seconds // 60) % 60
 
-
-def estimate(file_path: str) -> None:
+def get_data(file_path: Path, config: dict) -> tuple[int, int]:
     """
-    Calculate overtime or undertime based on work hours.
+    Extracts required and total work hours from a CSV file.
     """
-
     df = pd.read_csv(file_path)
     df = df.reset_index()  # make sure indexes pair with number of rows
 
     day_dict = {}
-    for index, row in df.iterrows():
+    for _, row in df.iterrows():
         day_dict[row["Date"]] = day_dict.get(row["Date"], 0) + row["Duration"]
 
     size = len(day_dict)
-    total_duration = sum(day_dict.values())
-    working_day_duration_h = 8
-    hours_to_seconds = 60 * 60
-    working_day_duration_s = working_day_duration_h * hours_to_seconds
-    required_duration = working_day_duration_s * size
+    total_duration = sum(day_dict.values()) - size * (
+        config.get("remove_breaks") * BREAK_TIME_SECONDS
+    )
+    required_duration = WORKING_DAY_DURATION_S * size
 
-    overtime = timedelta(seconds=int(total_duration - required_duration))
-    hours, minutes = hours_minutes(overtime)
+    return required_duration, total_duration
+
+
+def calc_overtime(required_duration, total_duration):
+    """
+    Calculate overtime.
+    """
+    td = timedelta(seconds=int(total_duration - required_duration))
+    hours, minutes = (
+        td.days * DAY_TO_HOURS + td.seconds // HOURS_TO_SECONDS,
+        (td.seconds // MIN_TO_SECONDS) % HOURS_TO_MINUTES,
+    )
+    return hours, minutes
+
+
+def estimate(file_path: str, config: dict) -> None:
+    """
+    Calculate overtime or undertime based on work hours.
+    """
+
+    required_duration, total_duration = get_data(file_path, config)
+
+    hours, minutes = calc_overtime(required_duration, total_duration)
     if total_duration > required_duration:
+        free_days = hours // WORKING_DAY_DURATION_H
         print(f"Overtime: {hours}:{minutes}:00")
-        print(f"Eligible free days: {hours // working_day_duration_h}")
+        print(f"Eligible free days: {free_days}")
         print(
-            f"Eligible not full day: {hours - working_day_duration_h*(hours // working_day_duration_h)}:{minutes}:00"
+            f"Eligible not full day: {hours - WORKING_DAY_DURATION_H*(free_days)}:{minutes}:00"
         )
 
     else:
@@ -51,10 +76,19 @@ def main():
         required=True,
         help="Path to the CSV file containing work hours.",
     )
+    parser.add_argument(
+        "-r",
+        "--remove-breaks",
+        action="store_true",
+        help="Remove breaks from the calculation.",
+    )
     args = parser.parse_args()
 
     file_path = Path(args.file).expanduser().resolve()
-    estimate(file_path)
+    config = {
+        "remove_breaks": 1 if args.remove_breaks else 0,
+    }
+    estimate(file_path, config)
 
 
 if __name__ == "__main__":
